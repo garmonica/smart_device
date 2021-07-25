47c27de4a562704836512cde62a2b57619b7c2a4
Olia <good-friday@mail.ru>
2021-07-25 18:44:05 +0300

    Реализация аккордеона

--- build/js/main.js
+++ build/js/main.js
@@ -1,7 +1,8 @@
 const mainScreen = document.querySelector('.main-screen');
-const mainScreenLinks = mainScreen.querySelectorAll('a');
 
 if (mainScreen) {
+  const mainScreenLinks = mainScreen.querySelectorAll('a');
+
   mainScreenLinks.forEach((link) => {
     link.addEventListener('click', (evt) => {
       evt.preventDefault();
@@ -10,6 +11,26 @@ if (mainScreen) {
         behavior: 'smooth',
         block: 'start',
       });
+    });
+  });
+}
+
+const accordion = document.querySelector('.footer-menu');
+
+if (accordion) {
+  const accordionBlocks = accordion.querySelectorAll('.footer-menu__block');
+  const accordionHeadings = accordion.querySelectorAll('.footer-menu__heading');
+
+  accordion.classList.remove('footer-menu--nojs');
+
+  accordionHeadings.forEach((heading, i) => {
+    heading.addEventListener('click', () => {
+      if (accordionBlocks[i].classList.contains('footer-menu__block--opened')) {
+        accordionBlocks[i].classList.remove('footer-menu__block--opened');
+      } else {
+        accordionBlocks.forEach((item) => item.classList.remove('footer-menu__block--opened'));
+        accordionBlocks[i].classList.add('footer-menu__block--opened');
+      }
     });
   });
 }
