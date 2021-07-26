c69bbab96a7b356431aff270adbcbae2317c9f3a
Olia <good-friday@mail.ru>
2021-07-26 20:38:57 +0300

    Вёрстка и реализация модального окна + localStorage

--- source/js/main.js
+++ source/js/main.js
@@ -34,3 +34,93 @@ if (accordion) {
     });
   });
 }
+
+const modal = document.querySelector('.modal');
+const modalOpen = document.querySelector('.header-top__button');
+
+if (modal) {
+  const modalForm = modal.querySelector('form');
+  const modalClose = modal.querySelector('.modal__close');
+  const modalName = modal.querySelector('input[type="text"]');
+  const modalPhone = modal.querySelector('input[type="tel"]');
+  const modalQuestion = modal.querySelector('textarea');
+
+  const overlay = document.createElement('div');
+  overlay.classList.add('overlay');
+
+  const openModal = () => {
+    modal.classList.add('modal--show');
+    document.body.appendChild(overlay);
+
+    if (!modalName.value) {
+      modalName.focus();
+    } else if (!modalPhone.value) {
+      modalPhone.focus();
+    } else {
+      modalQuestion.focus();
+    }
+  };
+
+  const closeModal = () => {
+    modal.classList.remove('modal--show');
+    document.body.removeChild(overlay);
+  };
+
+  modalOpen.addEventListener('click', () => openModal());
+
+  modalClose.addEventListener('click', () => closeModal());
+
+  overlay.addEventListener('click', () => closeModal());
+
+  window.addEventListener('keydown', (evt) => {
+    if (evt.key === ('Escape' || 'Esc')) {
+      if (modal.classList.contains('modal--show')) {
+        evt.preventDefault();
+        closeModal(modal);
+      }
+    }
+  });
+
+  modalForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    closeModal();
+  });
+}
+
+// Local Storage
+
+const forms = document.querySelectorAll('form');
+const inputsName = document.querySelectorAll('input[type="text"]');
+const inputsPhone = document.querySelectorAll('input[type="tel"]');
+const textareasQuestion = document.querySelectorAll('textarea');
+
+let isStorageSupport = true;
+let storageUserName = '';
+let storageUserPhone = '';
+let storageUserQuestion = '';
+
+try {
+  storageUserName = localStorage.getItem('user-name');
+  storageUserPhone = localStorage.getItem('user-phone');
+  storageUserQuestion = localStorage.getItem('user-question');
+} catch (err) {
+  isStorageSupport = false;
+}
+
+if (storageUserName || storageUserPhone || storageUserQuestion) {
+  inputsName.forEach((name) => name.value = storageUserName);
+  inputsPhone.forEach((phone) => phone.value = storageUserPhone);
+  textareasQuestion.forEach((text) => text.value = storageUserQuestion);
+}
+
+forms.forEach((form, i) => {
+  form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    if (isStorageSupport) {
+      localStorage.setItem('user-name', inputsName[i].value);
+      localStorage.setItem('user-phone', inputsPhone[i].value);
+      localStorage.setItem('user-question', textareasQuestion[i].value);
+    }
+  });
+});
